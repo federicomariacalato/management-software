8c8fd18fa4c0d39760d457343362fb470417159c
Federico Maria Calato <calatofederico48@gmail.com>
2026-09-02 17:49:41 +0200

    feat(routing): add base pages, layout and router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { DashboardLayout } from "./components/layout/DashboardLayout";
+import Dashboard from "./pages/Dashboard";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import Products from "./pages/Products";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route element={<DashboardLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/products" element={<Products />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/layout/DashboardLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export function DashboardLayout() {
+  return (
+    <>
+      <div>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+}
